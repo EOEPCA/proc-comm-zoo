f95a85cde7264018f150307bf983fa7f4bf6d7f2
Roberto  di Rienzo <roberto.dirienzo@terradue.com>
2020-03-25 16:24:20 +0100

    EOEPCA-40 #comment build scripts updated

--- scripts/build.sh
+++ scripts/build.sh
@@ -9,20 +9,28 @@ then
     exit 1
 fi
 
-#echo "RUN SCRIPTS"
-#docker run --rm --user root -v $PWD:/work -v /tmp/output:/output  ${DOCKERIMAGE} /work/scripts/zoo.sh
-#if [ $? -ne 0 ]
-#then
-#	echo "Build scripts failed"
-#	exit 2
-#fi
+chmod +x scripts/zoo.sh
 
-docker build --rm -t eoepca-zoo:1.0 .
+FOLDER=/work/build
+if [ ! -z "${RUNFOLDER}" ]
+then
+	FOLDER=${RUNFOLDER}
+fi
+
+echo "RUN SCRIPTS"
+docker run --rm --user root --env FOLDER=${FOLDER} -v $PWD:/work ${DOCKERIMAGE} /work/scripts/zoo.sh
 if [ $? -ne 0 ]
 then
-	echo "Docker image  failed"
-	exit 3
+	echo "Build scripts failed"
+	exit 2
 fi
+
+# docker build --rm -t eoepca-zoo:1.0 .
+# if [ $? -ne 0 ]
+# then
+# 	echo "build Docker image  failed"
+# 	exit 3
+# fi
 
 
 
