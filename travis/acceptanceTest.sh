925fdcf991ca0bd3e8cc0fc37e80220618ea30fd
Roberto  di Rienzo <roberto.dirienzo@terradue.com>
2020-05-04 23:12:23 +0200

    acceptanceTest updated

--- travis/acceptanceTest.sh
+++ travis/acceptanceTest.sh
@@ -48,5 +48,29 @@ then
 		exit 1
 	fi
 
+
+  echo "run longProcess  test"
+  job=$(curl -s  -v -L -X POST "http://localhost:7777/wps3/processes/longProcess/jobs" -H  "accept: application/json" -H  "Prefer: respond-async" -H  "Content-Type: application/json" -d "{\"inputs\":[{\"id\":\"string\"}],\"outputs\":[{\"format\":{\"mimeType\":\"string\",\"schema\":\"string\",\"encoding\":\"string\"},\"id\":\"Result\",\"transmissionMode\":\"value\"}]}"  2>&1 |  grep Location  | cut -d':' -f2)
+
+  echo 'run!'
+
+  echo "get Status:"
+  curl -s -L "http://localhost:7777/${job}" -H "accept: application/json"
+
+  echo  "Sleep 1s"
+  sleep 1
+
+  echo "get Status:"
+  curl -s -L "http://localhost:7777/${job}" -H "accept: application/json"
+
+  echo  "Sleep 20s"
+  sleep 20
+
+  echo "get Status:"
+  curl -s -L "http://localhost:7777/${job}" -H "accept: application/json"
+
+  echo "get result:"
+  curl -s -L "http://localhost:7777/${job}/result" -H "accept: application/json"
+
 fi
 
